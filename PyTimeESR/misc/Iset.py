ccd4c0b0ad70ff49e01df13bdbd5ec26e0cc074b
matysek00 <matyas.nachtigall@gmail.com>
2025-08-07 17:06:32 +0200

    comments

--- PyTimeESR/misc/Iset.py
+++ PyTimeESR/misc/Iset.py
@@ -1,19 +1,41 @@
 from scipy.optimize import root_scalar
 import numpy as np
 
-def Iset_step(gL, Itarget, Sim, outfile=None):
-
-    Sim.Dyn.params['gamma0'][1] = gL
-    Sim.run(outfile)
-    Sim.load_output()
-    DC = Sim.results_dict['DC']
-    return DC-Itarget
-
-
-def Iset(Sim, Itarget, outfile=None, frequency=None, init_step=None,
-         method='brentq'):#options={}):
-    # initial guess
+def Iset(Sim, 
+         Itarget: float, 
+         outfile: str = None, 
+         frequency: float = None, 
+         init_step: float = None,
+         method: str = 'brentq'):
+    """
+    Set the current in the simulation by adjusting the damping parameter gamma0.
+    Bound by 0, and the right damping parameter gamma0[0].
+    Similiar (with different optimization method) to the Iset function in the Fortran code.
     
+    Parameters
+    ----------
+    Sim : Simulation object
+        The simulation object containing the dynamic parameters.
+    Itarget : float
+        The target current to set in the simulation.
+    outfile : str, optional
+        Where to dump Fortran output, by default None. 
+    frequency : float, optional
+        The frequency of the driving force, by default None (no driving).
+        WARNING: A and B drive parameters are set to zero if frequency is None.
+    init_step : float, optional
+        Initial step size for the root finding method, by default None.
+    method : str, optional
+        The method to use for root finding, by default 'brentq'.
+    
+    Returns
+    -------
+    res : OptimizeResult
+        If suceesful, the result of the optimization containing the root and convergence status 
+        If not successful, a dictionary with the root and a message indicating failure.
+    """
+    
+    # initial guess
     gL = Sim.Dyn.params['gamma0'][1]
     gR = Sim.Dyn.params['gamma0'][0]
     bounds = (1e-8, gR)
@@ -21,6 +43,7 @@ def Iset(Sim, Itarget, outfile=None, frequency=None, init_step=None,
     
     if frequency is None:
         # turn off driving 
+        print('WARNING: No frequency provided, setting driving parameters to zero.')
         Sim.Dyn.params['A'] = [0+0j,0+0j]
         Sim.Dyn.params['bessel_amplitude'] = [0,0]
         Sim.Dyn.params['n_max'] = 3
@@ -35,3 +58,12 @@ def Iset(Sim, Itarget, outfile=None, frequency=None, init_step=None,
     except: 
         res = {'converged': False, 'root': gL, 'message': 'Failed with method: ' + method}
     return res
+
+
+def Iset_step(gL, Itarget, Sim, outfile=None):
+
+    Sim.Dyn.params['gamma0'][1] = gL
+    Sim.run(outfile)
+    Sim.load_output()
+    DC = Sim.results_dict['DC']
+    return DC-Itarget
