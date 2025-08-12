20e0651eff09c1b669ec7d928abb05545f065758
matysek00 <matyas.nachtigall@gmail.com>
2025-08-12 15:26:48 +0200

    added ratio in Iset

--- PyTimeESR/misc/Iset.py
+++ PyTimeESR/misc/Iset.py
@@ -6,7 +6,8 @@ def Iset(Sim,
          outfile: str = None, 
          frequency: float = None, 
          init_step: float = None,
-         method: str = 'brentq'):
+         method: str = 'brentq',
+         ratio: float = 1.):
     """
     Set the current in the simulation by adjusting the damping parameter gamma0.
     Bound by 0, and the right damping parameter gamma0[0].
@@ -27,6 +28,8 @@ def Iset(Sim,
         Initial step size for the root finding method, by default None.
     method : str, optional
         The method to use for root finding, by default 'brentq'.
+    ratio : float, optional
+        Ratio to scale the upper bound of gL/gR, by default 1.0.
     
     Returns
     -------
@@ -38,7 +41,7 @@ def Iset(Sim,
     # initial guess
     gL = Sim.Dyn.params['gamma0'][1]
     gR = Sim.Dyn.params['gamma0'][0]
-    bounds = (1e-8, gR)
+    bounds = (1e-8, ratio*gR)
     x1 = None if init_step is None else gL+init_step # secondary guess
     
     Itarget = np.abs(Itarget)  # ensure target current is positive
@@ -56,7 +59,7 @@ def Iset(Sim,
     #res = minimize(Iset_step, gL, (Itarget, Sim, outfile), bounds = bounds, options=options)
     try:
         res = root_scalar(Iset_step, args = (Itarget, Sim, outfile), 
-            method=method, bracket=bounds, x0=gL, x1=x1, xtol=1e-8)
+            method=method, bracket=bounds, x0=gL, x1=x1, xtol=1e-5)
     except: 
         res = {'converged': False, 'root': gL, 'message': 'Failed with method: ' + method}
     return res
@@ -69,7 +72,7 @@ def Iset_step(gL, Itarget, Sim, outfile=None):
     Sim.load_output()
     DC = np.abs(Sim.results_dict['DC'])
 
-    print('Iset_step: gamma0 =', gL, 'DC =', DC, 'Itarget =', Itarget, flush=True)
+    print('Iset_step: gammaL =', gL, 'DC =', DC, 'Itarget =', Itarget, flush=True)
     return DC-Itarget
 
 def Feedback(Sim, 
